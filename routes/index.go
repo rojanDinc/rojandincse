4d0daaf3def7c1069e70ecfce1dc685ffa0eae02
rojan <rojand94@gmail.com>
2024-10-11 22:26:05 +0200

    update template titles

--- routes/index.go
+++ routes/index.go
@@ -12,7 +12,7 @@ type IndexPage struct {
 
 func IndexHandler(template *template.Template) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if err := template.ExecuteTemplate(w, "index.html", IndexPage{Title: "rojandinc.se"}); err != nil {
+		if err := template.ExecuteTemplate(w, "index.html", IndexPage{Title: "Home"}); err != nil {
 			log.Println("failed to execute template: ", err)
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
